68a89865f883073b43f30359b712b1eb779f5cd0
Ashish Bhargav <ashishvarala@gmail.com>
2023-12-06 01:34:12 +0530

    fetching speeches from RBI website

--- financial-text-analysis.R
+++ financial-text-analysis.R
@@ -2,10 +2,29 @@ require("tm")
 require("data.table")
 require("dplyr")
 require("SentimentAnalysis")
+library("rvest")
+library("SnowballC")
+library("wordcloud")
+library("RColorBrewer")
 
-#load speech documents
-speech_corpus <- Corpus(DirSource("Downloads/fintech-group-6-fta/data/"))
-View(speech_corpus)
+# Function to scrape text from RBI website
+fetch_rbi_speeches <- function() {
+  base_url <- "https://rbi.org.in/Scripts/BS_SpeechesView.aspx"
+  speech_qty <- readline("enter the number of speeches: ")
+  speech_list <- list()
+  for (i in 1:speech_qty) {
+    id <- readline("enter a speech id: ")
+    
+    speech_url <- paste0(base_url, "?Id=", id)
+    
+    webpage <- read_html(speech_url)
+    paragraphs <- webpage %>% html_nodes(".td p:not(.footnote)") %>% html_text()
+    text <- paste(paragraphs, collapse = "\n")
+    speech_list[[i]] <- text
+  }
+  speech_corpus <- Corpus(VectorSource(speech_list))
+  return(speech_corpus)
+}
 
 #Function to preprocess the text corpus
 # Stemming/Lemmatization is optional in preprocessing
@@ -38,25 +57,49 @@ preprocess_text <- function(text_corpus){
 #Mean sentiment of all sentences is then taken to calculate speech sentiment
 #Iteratively looped over the entire corpus to calculate net sentiment
 calculate_sentiment <- function(corpus){
-  preprocessed_corpus <- preprocess_text(corpus)
-
-  net_sentiment <- rep(NA, length(preprocessed_corpus))
-
-  for (i in 1:length(preprocessed_corpus)) {
-    speech_sentences = sentimentr::get_sentences(as.character(preprocessed_corpus[[i]][1]))
-
-    #The loughran-mcdonald lexicon is used to assign weights to each word in a sentence
-    #Valence shifters have separate weights given in the hash_valence_shifters table
-    speech_sentence_sentiment = sentimentr::sentiment(speech_sentences,lexicon::hash_sentiment_loughran_mcdonald,
-                                                     valence_shifters_dt = lexicon::hash_valence_shifters,
-                                                     n.before = Inf, n.after = Inf)
-
-    net_sentiment[i] = mean(speech_sentence_sentiment$sentiment)
+  
+  net_sentiment <- rep(NA, length(corpus))
+  if(length(corpus)>0){
+    for (i in 1:length(corpus)) {
+      speech_sentences = sentimentr::get_sentences(as.character(corpus[[i]][1]))
+      
+      #The loughran-mcdonald lexicon is used to assign weights to each word in a sentence
+      #Valence shifters have separate weights given in the hash_valence_shifters table
+      speech_sentence_sentiment = sentimentr::sentiment(speech_sentences,lexicon::hash_sentiment_loughran_mcdonald,
+                                                        valence_shifters_dt = lexicon::hash_valence_shifters,
+                                                        n.before = Inf, n.after = Inf)
+      
+      net_sentiment[i] = mean(speech_sentence_sentiment$sentiment)
+    }
+    return(net_sentiment)
   }
+  else{
+    return(net_sentiment)
+  }
+}
+
+generate_wordcloud <- function(corpus){
+  document_matrix <- tm::TermDocumentMatrix(corpus)
+  text_matrix <- as.matrix(document_matrix)
+  v <- sort(rowSums(text_matrix),decreasing=TRUE)
+  d <- data.frame(word = names(v),freq=v)
+  head(d, 10)
+  
+  set.seed(1234)
+  wordcloud(words = d$word, freq = d$freq, min.freq = 1,
+            max.words=200, random.order=FALSE, rot.per=0.35, 
+            colors=brewer.pal(8, "Dark2"))
+}
+
+financial_sentiment_main <- function(){
+  speech_corpus <- fetch_rbi_speeches()
+  preprocessed_corpus <- preprocess_text(speech_corpus)
+  net_sentiment <- calculate_sentiment(preprocessed_corpus)
+  generate_wordcloud(preprocessed_corpus)
   return(net_sentiment)
 }
 
-calculate_sentiment(speech_corpus)
+print(financial_sentiment_main())
 
 #---------------Sanity Test-------------#
 View(preprocess_text(speech_corpus))
